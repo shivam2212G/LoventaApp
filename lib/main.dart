f39082ffab1b0953f1693028476d0fbb979294ad
Shivam <bulletspeed12345@gmail.com>
2025-07-22 22:37:10 +0530

    Loventa Fifth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '2 Demo Home Pag e'),
+      home: const MyHomePage(title: '5 Demo Home Pag e'),
     );
   }
 }
